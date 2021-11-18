715495eeb853ca993d724090f246ff035cd1f7a4
Nicault <92713918+Nicault@users.noreply.github.com>
2021-11-18 12:38:56 +0100

    message de confirmation

--- js/modal.js
+++ js/modal.js
@@ -12,12 +12,41 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+const closeModalButton = document.querySelector(".close")
+const submitButton = document.querySelector(".btn-submit")
+
+
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal event
+closeModalButton.addEventListener("click", closeModal)
+
+
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
+
+
+function messageConfirmation(e) {
+  e.preventDefault() 
+  const modalBody = document.querySelector(".modal-body")
+    if (modalBody) {
+    modalBody.innerHTML = "<div>Merci ! Votre réservation a été reçue.</div><button class='btn-submit button flex-end' id='btn-close'>Fermer</button>"
+    modalBody.classList.add = ".modal-body-after"
+    document.querySelector("#btn-close").addEventListener("click", closeModal)
+    }
+}
+// submit form
+submitButton.addEventListener("click", messageConfirmation)
 
 
